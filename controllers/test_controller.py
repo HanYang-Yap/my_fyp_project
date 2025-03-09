e11aade59f79108d710fc8040c89f60d6d59ba4c
Henry_hy <117017994+HanYang-Yap@users.noreply.github.com>
2025-03-09 17:40:52 +0800

    initial commit on henryhy_branch

--- controllers/test_controller.py
+++ controllers/test_controller.py
@@ -36,7 +36,7 @@ def ask_openai():
 # 🔹 測試首頁
 @test_bp.route('/')
 def test():
-    return 'Hello, this is the index page!'
+    return 'One In A Million, annyeonghaseyo, this is the index page!'
 
 # 🔹 測試 home 頁面 (渲染 HTML)
 @test_bp.route('/home')
