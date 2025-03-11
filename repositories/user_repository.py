e9ce67d049be54930e05be1ae1ad2960d3af8839
Henry_hy <117017994+HanYang-Yap@users.noreply.github.com>
2025-03-11 16:23:09 +0800

    Update user_repository.py
    some changes in user_repository

--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -7,20 +7,20 @@ class UserRepository:
         self.collection = self.db.collection('users')  # Firestore Collection "users"
 
     def add_user(self, user):
-        """將 User 存入 Firestore"""
+        """Create User 存入 Firestore"""
         self.collection.document(user.user_id).set(user.to_dict())
 
     def get_user(self, user_id):
-        """從 Firestore 讀取 User"""
+        """從 Firestore 讀取read User"""
         doc = self.collection.document(user_id).get()
         if doc.exists:
             return User.from_dict(doc.to_dict())  # 使用 `from_dict()` 轉換
         return None
 
     def update_user(self, user):
-        """更新 User 資料"""
+        """更新Update User 資料"""
         self.collection.document(user.user_id).update(user.to_dict())
 
     def delete_user(self, user_id):
-        """刪除 User"""
+        """刪除Delete User"""
         self.collection.document(user_id).delete()
