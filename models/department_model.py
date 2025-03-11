0f320ef747a76f796b8f672dd305e999bc09a1cc
Henry_hy <117017994+HanYang-Yap@users.noreply.github.com>
2025-03-12 01:05:59 +0800

    add new department_model.py

---
+++ models/department_model.py
@@ -0,0 +1,7 @@
+
+class Department:
+
+    def __init__(self, department_id, department_name, school_name):
+        self.department_id = department_id
+        self.department_name = department_name
+        self.school_name = school_name
