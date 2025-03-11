ee98c711f9b2d1c5c040038c006da83d07d53782
Henry_hy <117017994+HanYang-Yap@users.noreply.github.com>
2025-03-11 17:50:53 +0800

    commit changes in the user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -1,5 +1,5 @@
 class User:
-    def __init__(self, user_id, name, email):
+    def __init__(self, user_id, name, email, password_hash, role_id, department_id, created_at, updated_at):
         """
         初始化 User 物件
         :param user_id: 使用者 ID (Firebase 的 document ID)
@@ -9,6 +9,11 @@ class User:
         self.user_id = user_id
         self.name = name
         self.email = email
+        self.password_hash = password_hash
+        self.role_id = role_id
+        self.department_id = department_id
+        self.created_at = created_at
+        self.updated_at = updated_at
 
     def to_dict(self):
         """
@@ -17,7 +22,12 @@ class User:
         return {
             "user_id": self.user_id,
             "name": self.name,
-            "email": self.email
+            "email": self.email,
+            "password_hash": self.password_hash,
+            "role_id": self.role_id,
+            "department_id": self.department_id,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
         }
 
     @staticmethod
@@ -30,5 +40,10 @@ class User:
         return User(
             user_id=data.get("user_id"),
             name=data.get("name"),
-            email=data.get("email")
+            email=data.get("email"),
+            password_hash=data.get("password_hash"),
+            role_id=data.get("role_id"),
+            department_id=data.get("department_id"),
+            created_at=data.get("created_at"),
+            updated_at=data.pygame.event.get()("updated_at")
         )
